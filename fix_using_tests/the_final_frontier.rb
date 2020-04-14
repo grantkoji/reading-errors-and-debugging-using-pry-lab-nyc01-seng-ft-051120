f260cb48b9632ee4c69e9f27df496cfc8b7346f9
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-14 21:05:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,16 @@
 # don't forget to add: require 'pry'
+star_date = 0
 
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+  star_date = (rand(100000) + 400000) / 10.0
 end
 
+generate_star_date
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
-  puts state_log(date)
-  date = generate_star_date
+def engage(star_date)
+  return "/Captain's Log, ${star_date} /"
 end
